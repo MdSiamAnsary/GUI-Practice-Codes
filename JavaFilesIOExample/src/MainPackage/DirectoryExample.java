5a2ee8341315fd6260a48cfafc2f567ff17ed96f
Md. Siam Ansary <siamansary.cse@gmail.com>
2020-05-11 02:37:15 +0600

    Java Files and I/O Practice

---
+++ JavaFilesIOExample/src/MainPackage/DirectoryExample.java
@@ -0,0 +1,32 @@
+
+package MainPackage;
+
+import java.io.File;
+
+public class DirectoryExample {
+    
+    public static void main(String[] args) 
+    {
+        
+        // mkdir example 
+        String dirname1 = "./oneDirectory";                     // path
+        File file1 = new File(dirname1);                        // file
+        file1.mkdir();                                          // make directory
+        
+        
+        // mkdirs example 
+        String dirname2 = "./parentDirectory/oneDirectory/twoDirectory";    // path
+        File file2 = new File(dirname2);                                    // file
+        file2.mkdirs();                                                     // make directory with parent directory
+        
+        
+        File file3 = new File("./parentDirectory");
+        String[] paths = file3.list();
+        for(String path:paths) 
+        {
+            System.out.println(path);
+        }
+        
+        
+    }
+}
