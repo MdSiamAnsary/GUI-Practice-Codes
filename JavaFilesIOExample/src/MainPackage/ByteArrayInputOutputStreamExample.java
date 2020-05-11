67b54a6d69f59eebb60b309b9b9d7844e43583ba
Md. Siam Ansary <siamansary.cse@gmail.com>
2020-05-11 06:02:43 +0600

    Java Files and I/O Practice

---
+++ JavaFilesIOExample/src/MainPackage/ByteArrayInputOutputStreamExample.java
@@ -0,0 +1,45 @@
+
+package MainPackage;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author Ansary
+ */
+public class ByteArrayInputOutputStreamExample {
+    //https://www.tutorialspoint.com/java/java_bytearrayinputstream.htm
+    public static void main(String args[])throws IOException 
+    {
+      ByteArrayOutputStream bOutput = new ByteArrayOutputStream(12);
+
+      while( bOutput.size()!= 10 ) 
+      {
+         // Gets the inputs from the user
+         bOutput.write("hello".getBytes()); 
+      }
+      byte b [] = bOutput.toByteArray();
+      System.out.println("Print the content");
+      
+      for(int x = 0 ; x < b.length; x++) {
+         // printing the characters
+         System.out.print((char)b[x]  + "   "); 
+      }
+      System.out.println("   ");
+      
+      int c;
+      ByteArrayInputStream bInput = new ByteArrayInputStream(b);
+      System.out.println("Converting characters to Upper case " );
+      
+      for(int y = 0 ; y <= 1; y++) {
+         while(( c = bInput.read())!= -1) {
+            System.out.println(Character.toUpperCase((char)c));
+         }
+         bInput.reset(); 
+         // https://www.tutorialspoint.com/java/io/bytearrayinputstream_reset.htm
+      }
+   }
+    
+}
---
+++ JavaFilesIOExample/src/MainPackage/DataInputOutputStreamExample.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainPackage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author Ansary
+ */
+public class DataInputOutputStreamExample {
+    
+    // Link : https://www.tutorialspoint.com/java/java_datainputstream.htm
+    
+    public static void main(String args[])throws IOException 
+    {
+
+      // writing string to a file encoded as modified UTF-8
+      DataOutputStream dataOut = new DataOutputStream(new FileOutputStream("./datafile.txt"));
+      dataOut.writeUTF("hello world");
+      dataOut.writeUTF("hello earth");
+
+      // Reading data from the same file
+      DataInputStream dataIn = new DataInputStream(new FileInputStream("./datafile.txt"));
+
+      while(dataIn.available()>0) 
+      {
+         String k = dataIn.readUTF();
+         System.out.print(k+"\n");
+      }
+   }
+    
+}
