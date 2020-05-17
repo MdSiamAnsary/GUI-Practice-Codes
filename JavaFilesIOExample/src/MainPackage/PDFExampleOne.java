5ea4e5270751156775fcdf03132da47cf682c691
Md. Siam Ansary <siamansary.cse@gmail.com>
2020-05-17 09:37:33 +0600

    PDF File Creation Example

--- JavaFilesIOExample/src/MainPackage/PDFExampleOne.java
+++ JavaFilesIOExample/src/MainPackage/PDFExampleOne.java
@@ -2,6 +2,9 @@
 package MainPackage;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
@@ -18,9 +21,15 @@ public class PDFExampleOne
         makeDirCurrTime();
         
         try{
-            Document documentObject = new Document();
+           
+            Document documentObject = new Document(PageSize.A4,72, 72, 72, 72);
             PdfWriter.getInstance(documentObject, new FileOutputStream(pdfFileLoc));
             documentObject.open();
+            
+           
+            Font bold = FontFactory.getFont(FontFactory.TIMES_BOLD, 12, Font.BOLD);
+         
+            documentObject.add(new Paragraph("Hello",bold));
             documentObject.add(new Paragraph("Hello"));
             documentObject.close();
             
